4037f5bbc707f85729f10a7cc801f3cb5d475b6e
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-04-29 20:25:53 +0300

    New updates made to the project

--- app/src/main/java/com/example/locationpractical/MainActivity.kt
+++ app/src/main/java/com/example/locationpractical/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.locationpractical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.locationpractical.databinding.ActivityMainBinding
+import com.example.locationpractical.service.LocationTracker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateLocation()
+    }
+
+    private fun updateLocation() {
+        if (LocationTracker(this).canGetLocation) {
+            binding.latti.setText(LocationTracker(this).getLatitude().toString())
+            binding.longi.setText(LocationTracker(this).getLongitude().toString())
+        }
     }
 }
 
